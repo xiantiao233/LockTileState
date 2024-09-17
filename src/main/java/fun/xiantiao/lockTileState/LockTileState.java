1e53b2ff63b6ab6e4205bb80afd864369dad25e1
xiantiao <3054587546@qq.com>
2024-09-17 10:47:51 +0800

    Initial commit

---
+++ src/main/java/fun/xiantiao/lockTileState/LockTileState.java
@@ -0,0 +1,17 @@
+package fun.xiantiao.lockTileState;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class LockTileState extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
