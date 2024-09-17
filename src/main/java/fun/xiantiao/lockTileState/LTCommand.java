26b4656c8b4b6f07f0f58a18f0cda9cf2c85c01c
xiantiao <3054587546@qq.com>
2024-09-17 11:00:05 +0800

    feat: register command class

---
+++ src/main/java/fun/xiantiao/lockTileState/LTCommand.java
@@ -0,0 +1,22 @@
+package fun.xiantiao.lockTileState;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.command.TabCompleter;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.List;
+
+public class LTCommand implements CommandExecutor, TabCompleter {
+    @Override
+    public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String s, @NotNull String[] args) {
+        return false;
+    }
+
+    @Override
+    public @Nullable List<String> onTabComplete(@NotNull CommandSender sender, @NotNull Command command, @NotNull String s, @NotNull String[] args) {
+        return List.of();
+    }
+}
--- src/main/java/fun/xiantiao/lockTileState/LockTileState.java
+++ src/main/java/fun/xiantiao/lockTileState/LockTileState.java
@@ -1,13 +1,17 @@
 package fun.xiantiao.lockTileState;
 
+import org.bukkit.command.PluginCommand;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class LockTileState extends JavaPlugin {
 
     @Override
     public void onEnable() {
-        // Plugin startup logic
-
+        PluginCommand lt = getCommand("lt");
+        if (lt != null) {
+            lt.setExecutor(new LTCommand());
+            lt.setTabCompleter(new LTCommand());
+        }
     }
 
     @Override
